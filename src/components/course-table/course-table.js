e3da7c95097a24f4d20e1a45ccdfdd4e181ad876
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-02-15 13:21:20 -0500

    editting course-table.js

--- src/components/course-table/course-table.js
+++ src/components/course-table/course-table.js
@@ -4,6 +4,36 @@ import CourseRow from "./course-row";
 export default class CourseTable
     extends React.Component {
 
-    
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>Course Table</h2>
+                <table className="table">
+                    <tbody>
+                    {
+                        this.props.courses.map((course, ndx) =>
+                            <CourseRow
+                                deleteCourse={this.props.deleteCourse}
+                                key={ndx}
+                                course={course}
+                                title={course.title}
+                                owner={course.owner}
+                                lastModified={course.lastModified}
+                        />)
+                    }
+                    </tbody>
+                </table>
+
+            </div>
+
+
+
+        )
+
+    }
 
 }
