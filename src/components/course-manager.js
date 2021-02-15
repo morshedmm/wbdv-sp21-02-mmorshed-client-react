92e51560dfa63a3f0af22e00bb94907225464ba9
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-02-15 13:36:43 -0500

    editting course-manager.js

--- src/components/course-manager.js
+++ src/components/course-manager.js
@@ -1,1 +1,21 @@
 import React from 'react'
+import CourseTable from "course-table/course-table";
+
+class CourseManager extends React.Component {
+    state = {
+        courses: [
+            {title: "CS1234", owner: "frank", lastModified: "2/9/15"},
+            {title: "CS2345", owner: "greg", lastModified: "3/8/25"},
+            {title: "CS3456", owner: "herbert", lastModified: "4/7/35"},
+            {title: "CS4567", owner: "ian", lastModified: "5/6/45"},
+
+
+        ]
+
+    }
+
+    
+
+}
+
+export default CourseManager
