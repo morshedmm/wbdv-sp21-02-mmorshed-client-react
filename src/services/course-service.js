ab6af9ea51767119f5924ebe1bd5ace06338c060
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-02-18 21:46:41 -0500

    modifying course services

--- src/services/course-service.js
+++ src/services/course-service.js
@@ -1,29 +1,41 @@
 
-function CourseServiceClient() {
+    const COURSES_URL =  'https://wbdv-generic-server.herokuapp.com/api/mmorshed/courses';
 
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/mmorshed/courses';
-    var self = this;
+export const findAllCourses = () =>
+    fetch(COURSES_URL)
+        .then(response => response.json())
 
-function createCourse(course) {
+export const deleteCourse = (courseId) =>
+    fetch(`${COURSES_URL}/${courseId}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
 
+export const createCourse = (course) =>
+    fetch(COURSES_URL, {
+        method: 'POST',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const updateCourse = (courseId, course) =>
+    fetch(`${COURSES_URL}/${courseId}`, {
+        method: 'PUT',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export default {
+    findAllCourses,
+    deleteCourse: deleteCourse,
+    createCourse,
+    updateCourse: updateCourse
 }
 
-function findAllCourses() {
 
-}
-
-function findCourseById(id) {
-
-}
-
-function updateCourse(id, course) {
-
-}
-
-function deleteCourse(id) {
-
-}
-
-
-
-}
