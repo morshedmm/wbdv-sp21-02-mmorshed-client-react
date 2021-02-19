e38017273da17d7e8d4c0dfdb5a72801bf0fa624
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-02-18 21:29:27 -0500

    added home.js and modified App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 import CourseManager from "./components/course-manager";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/home"
 
 function App() {
   return (
-    <div className="container-fluid">
-        <CourseManager/>
-    </div>
+      <BrowserRouter>
+        <div className="container-fluid">
+            <Route path="/" exact={true}>
+                <Home/>
+            </Route>
+            <Route path="/courses">
+                <CourseManager/>
+            </Route>
+        </div>
+      </BrowserRouter>
   );
 }
 
