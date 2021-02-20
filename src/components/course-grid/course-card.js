51b94bef95497ea5663cb8f53839064ff69f850f
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-02-20 10:37:47 -0500

    added button back as needed for card

--- src/components/course-grid/course-card.js
+++ src/components/course-grid/course-card.js
@@ -24,16 +24,17 @@ const CourseCard = ({course, deleteCourse, updateCourse, title}) => {
   return (
   <div className="col-4">
     <div className="card">
+
       <img src="https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
       className="card-img-top" alt="..."/>
+
       <div className="card-body">
+
 
         {
         !editing &&
-        <Link to="/courses/editor">
-        {/*<h5 className="card-title">{course.title}</h5>*/}
-        {title}
-        </Link>
+
+        <h5 className="card-title">{title}</h5>
         }
 
         {
@@ -47,16 +48,18 @@ const CourseCard = ({course, deleteCourse, updateCourse, title}) => {
         <p className="card-text">Some description</p>
         {/*<a href="#" className="btn btn-primary">Go somewhere</a>*/}
         <img src={``}/>
-        {/*
+        {
         <Link to="/courses/editor" className="btn btn-primary">
                     {course.title}
         </Link>
-        */}
+        }
         {/*<i className="fas fa-trash"></i>*/}
         {/*<i onClick={() => deleteCourse(course)} className="fas fa-trash"></i>*/}
+        <span className="float-right">
         {editing && <i onClick={() => deleteTitle()} className="fas fa-2x fa-trash"></i>}
         {!editing && <i onClick={() => setEditing(true)} className="fas fa-2x fa-edit"></i>}
         {editing && <i onClick={() => saveTitle()} className="fas fa-2x fa-check"></i>}
+        </span>
       </div>
     </div>
   </div> )
