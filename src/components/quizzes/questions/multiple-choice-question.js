1df034764caefba571f203b3e34bdbd76367586f
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-04-04 17:11:17 -0400

    styling fixes for MCQ

--- src/components/quizzes/questions/multiple-choice-question.js
+++ src/components/quizzes/questions/multiple-choice-question.js
@@ -4,14 +4,18 @@ const MultipleChoiceQuestion = ({question}) => {
     return(
         <div>
             <h4>{question.question}</h4>
-            {question.correct}
+            {/*question.correct*/}
             {
                 question.choices.map((choice) => {
                     return(
+                    <div>
                         <label>
                             <input type="radio" name={question._id}/>
-                            {choice}
+                            <span className="add-padding-left">
+                                {choice}
+                            </span>
                         </label>
+                    </div>
                     )
                 })
             }
