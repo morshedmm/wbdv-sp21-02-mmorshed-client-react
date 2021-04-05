7f840ae015b00cf5df60d48e6f91216f16c8f652
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-04-05 06:36:26 -0400

    modifying true false

--- src/components/quizzes/questions/true-false-question.js
+++ src/components/quizzes/questions/true-false-question.js
@@ -1,33 +1,98 @@
 import React, {useState} from "react";
 
 const TrueFalseQuestion = ({question}) => {
-    const [answer, setAnswer] = useState(null)
+    const [answer, setAnswer] = useState(null);
+    const [graded, setGraded] = useState(false);
     return (
         <div>
             <h4>
                 {question.question}
-                {
+                {/*
                     answer == question.correct &&
                     <i className="fas fa-check"></i>
-                }
-                {
+                */}
+                {/*
                     answer != question.correct &&
                     <i className="fas fa-times"></i>
-                }
+                */}
             </h4>
-            {question.correct}
+            {/*question.correct*/}
+            {/*<br/>*/}
+            {/*JSON.stringify(answer)*/}
             <br/>
-            {JSON.stringify(answer)}
-            <br/>
-            <label><input
-                type="radio"
-                onClick={() => setAnswer(true)}
-                name={question._id}/>True</label>
-            <br/>
-            <label><input
-                type="radio"
-                onClick={() => setAnswer(false)}
-                name={question._id}/>False</label>
+            <div className="list-group">
+
+              {!graded &&
+              <div className="list-group-item">
+                <label><input
+                    type="radio"
+                    onClick={() => setAnswer(true)}
+                    name={question._id}/>True</label>
+              </div>
+              }
+
+              {!graded &&
+              <div className="list-group-item">
+                <label><input
+                    type="radio"
+                    onClick={() => setAnswer(false)}
+                    name={question._id}/>False</label>
+              </div>
+              }
+
+              {graded && question.correct=='true' &&
+                <div className="list-group-item background-correct">
+                              <label><input
+                                  type="radio"
+                                  onClick={() => setAnswer(true)}
+                                  name={question._id}/>True</label>
+                </div>
+              }
+
+              {graded && question.correct!='false' &&
+                                            <div className="list-group-item">
+                                                                        <label><input
+                                                                            type="radio"
+                                                                            onClick={() => setAnswer(false)}
+                                                                            name={question._id}/>False</label>
+                              </div>
+              }
+
+              {graded && question.correct!='true' &&
+                                            <div className="list-group-item">
+                                                          <label><input
+                                                              type="radio"
+                                                              onClick={() => setAnswer(true)}
+                                                              name={question._id}/>True</label>
+                              </div>
+                            }
+
+              {graded && question.correct=='false' &&
+                <div className="list-group-item background-correct">
+                                            <label><input
+                                                type="radio"
+                                                onClick={() => setAnswer(false)}
+                                                name={question._id}/>False</label>
+                </div>
+              }
+
+
+
+
+
+
+
+            </div>
+
+                  <div>
+                     Your Answer: {answer}
+                  </div>
+                  <div>
+                            <i type="button" className="btn btn-success" onClick={() => setGraded(true)}>
+                                Grade
+                            </i>
+                  </div>
+
         </div>
     )
 }
