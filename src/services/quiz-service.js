38a42b561e0ca88feb9429bd69fac217443c44be
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-04-14 22:13:20 -0400

    remote database working

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,5 +1,5 @@
 //const QUIZZES_URL = "http://localhost:3000/api/quizzes";
-const QUIZZES_URL = "http://mmorshed-node.herokuapp.com/api/quizzes";
+const QUIZZES_URL = "https://mmorshed-node.herokuapp.com/api/quizzes";
 
 export const findQuizById = (quizId) =>
     fetch(`${QUIZZES_URL}/${quizId}`)
